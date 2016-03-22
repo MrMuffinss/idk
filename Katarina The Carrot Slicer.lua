296b8b2b7606a4b2448498bc8f29f79aaf59bb7d
MrMuffinss <smlork@gmail.com>
2016-03-22 02:06:36 +0000

    Update Katarina The Carrot Slicer.lua

--- Katarina The Carrot Slicer.lua
+++ Katarina The Carrot Slicer.lua
@@ -1,12 +1,13 @@
 if GetObjectName(myHero) ~= "Katarina" then return end
 
- require "Inspired"
+ require ("Inspired")
 
  -- Menu
  Katarina = Menu("Katarina", "Katarina")
  -- Katarina Combo Menu
  Katarina:SubMenu("Combo", "Combo")
  Katarina.Combo:Boolean("Q", "Use Q", true)
+ Katarina.Combo:Boolean("W", "Use W", true)
  Katarina.Combo:Boolean("E", "Use E", true)
  Katarina.Combo:Boolean("R", "Use R", true)
 
@@ -14,16 +15,16 @@ if GetObjectName(myHero) ~= "Katarina" then return end
 
 OnTick(function(myHero)
 if rChan == true then return end
-	if IOW:Mode() == 'Combo' then
-	Combo()
-end
+ if IOW:Mode() == "Combo" then
+  Combo()
+ end
 end)
 
- OnUpdateBuff (function(unit, buff)
- if not unit or not buff then
+ OnUpdateBuff (function(myHero, buff)
+ if not myHero or not buff then
   return
  end
- if buff.Name == "grievouswound" then
+ if buff.Name == "katarinarsound" then
   print("R Casting spells and movements blocked!")
   IOW.movementEnabled = false
   IOW.attacksEnabled = false
@@ -33,11 +34,11 @@ end)
     end
 end)
 
-OnRemoveBuff (function(unit, buff)
- if not unit or not buff then
+OnRemoveBuff (function(myHero, buff)
+ if not myHero or not buff then
   return
  end
- if buff.Name == "grievouswound" then
+ if buff.Name == "katarinarsound" then
   print("R Casting spells and movements unblocked!")
   IOW.movementEnabled = true
   IOW.attacksEnabled = true
@@ -48,24 +49,30 @@ OnRemoveBuff (function(unit, buff)
 end)
 
 function Combo()
-	local unit = GetCurrentTarget()
-	if Katarina.Combo.Q:Value() then
-		if Ready(_Q) and ValidTarget(unit, 675) then
-			CastTargetSpell(unit, _Q)
-		end
-	end
+ local unit = GetCurrentTarget()
+ if Katarina.Combo.Q:Value() then
+  if Ready(_Q) and ValidTarget(unit, 675) then
+   CastTargetSpell(unit, _Q)
+  end
+ end
 
-	if Katarina.Combo.E:Value() then
-		if Ready(_E) and ValidTarget(unit, 700) and  CanUseSpell(myHero, _Q) ~= READY then
-			CastTargetSpell(unit, _E)
-		end
-	end
+ if Katarina.Combo.E:Value() then
+  if Ready(_E) and ValidTarget(unit, 700) and CanUseSpell(myHero, _Q) ~= READY then
+   CastTargetSpell(unit, _E)
+  end
+ end
 
-	if Katarina.Combo.R:Value() then
-		if Ready(_R) and ValidTarget(unit, 550) then
-			CastTargetSpell(myHero, _R)
-		end
-	end
+ if Katarina.Combo.W:Value() then
+  if Ready(_W) and ValidTarget(unit, 375) then
+   CastTargetSpell(myHero, _W)
+  end
+ end
+
+ if Katarina.Combo.R:Value() then
+  if Ready(_R) and ValidTarget(unit, 550) then
+   CastTargetSpell(myHero, _R)
+  end
+ end
 
 end
 
